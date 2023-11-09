76d1c18bfac31b3f1a74bf69bffedf349991ca8f
unit098 <williamrobot1@gmail.com>
2023-11-09 15:07:07 -0700

    preping for vm reinstall

--- lcdLib/lcddemo.c
+++ lcdLib/lcddemo.c
@@ -4,9 +4,23 @@
 
 #include <libTimer.h>
 #include "lcdutils.h"
-#include "lcddraw.h"
+#in
+clude "lcddraw.h"
 
 /** Initializes everything, clears the screen, draws "hello" and a square */
+void
+f1(){
+  for (int i = 0; i<30; i++){
+    drawPixel(i, i, COLOR_PINK);
+    drawPixel(i, 30-i , COLOR_PINK);
+  }
+}
+void
+f2(){
+  for (int i = 0; i<30; i++)
+    drawPixel(i,30-i , COLOR_PINK);
+}
+
 int
 main()
 {
@@ -16,8 +30,10 @@ main()
 
   clearScreen(COLOR_BLUE);
 
-  drawString5x7(20,20, "hello", COLOR_GREEN, COLOR_RED);
+  // drawString5x7(20,20, "hello", COLOR_GREEN, COLOR_RED);
 
-  fillRectangle(30,30, 60, 60, COLOR_ORANGE);
+  // fillRectangle(30,30, 60, 60, COLOR_ORANGE);
+  f1();
+  // f2();
   
 }
